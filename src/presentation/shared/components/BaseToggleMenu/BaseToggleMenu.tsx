e784b16fed02c19d7911f5eb8db597e89eccbd5b
KyLan-Le <kylan@rev.vn>
2025-01-11 14:16:23 +0700

    update menu toggle icon

--- src/presentation/shared/components/BaseToggleMenu/BaseToggleMenu.tsx
+++ src/presentation/shared/components/BaseToggleMenu/BaseToggleMenu.tsx
@@ -4,6 +4,12 @@ import styled from "styled-components";
 const BaseToggleMenu = styled(Button)`
   background: transparent;
   border: 0;
+  display: flex;
+  justify-content: end;
+
+  svg {
+    width: 12px;
+  }
 `;
 
 export default BaseToggleMenu;
